bc5e79b72f153518a92490a13d69dc4a898db447
Michael Eaton II <Thrilshadow@Michaels-MacBook-Pro.local>
2013-01-13 23:44:29 -0500

    Added another while loop.

--- Eaton-Michael-project2.js
+++ Eaton-Michael-project2.js
@@ -7,7 +7,7 @@
 
 // Variables
 
-var leader = "\"Dragon Slayer\ Lunrshift"
+var leader = "\"Dragon Slayer\" Lunrshift"
 var raidMembers = 10
 var coleader = "JohnyB"
 var boss = "Onxyia"
@@ -24,14 +24,32 @@ console.log(leader + " initiates a Ready Check.");
 console.log("The Ready Cheack outputs " + readyCheck + ".");
 console.log("Ok" + boss + " has three phases. Phase one will be a simple ground phase. Phase 2 is a sky phase.");
 console.log("In this phase range dps needs to be on " + boss + " and melee needs to be on " + adds + " control.");
-console.log(" In phase 3 " + boss + " in back on the ground she will fear so everyone stay away from the " + adds + " caves."); 
+console.log("In phase 3 " + boss + " in back on the ground she will fear so everyone stay away from the " + adds + " caves."); 
 console.log("Once we get " + boss + " too 0% we win and we get loot.");
 console.log("Ok we are ready and know the fight so let's get her down.")
 
-//while loop
-while (health > 0) {
+//while loop 75%
+
+while (health > 75) {
     console.log("She is down to " + health + "% health.");
-    health-=15;
+    health-=5;
+};
+    console.log(boss + " down to " + health + " health. She is lifting off.");   
+   
+    
+
+
+    
+    
+    
+//While loop 50%
+while (  health > 50) {
+    console.log("she id down to" + health + " % health.");
+     health-=5;
 };
     
-console.log("Woot!!! Good job team! Time to roll for loot.");
+    
+    
+    
+    
+//console.log("Woot!!! Good job team! Time to roll for loot.");
