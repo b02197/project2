256a1b0be2e18e5086df22106b1e4e6034e2515e
Michael Eaton II <Thrilshadow@Michaels-MacBook-Pro.local>
2013-01-14 18:11:31 -0500

    reorginized code and deleted some code.

--- Eaton-Michael-project2.js
+++ Eaton-Michael-project2.js
@@ -1,11 +1,11 @@
-//alert("JavaScript works!");
+//alert("/*JavaScript works!");
 
 //Michael Eaton
 //SDI 1301
-// Project 2
-//
+//Project 2
 
-// Variables
+
+//Variables
 
 var leader = "\"Dragon Slayer\" Lunrshift"
 var raidMembers = 8
@@ -27,17 +27,23 @@ console.log("Once we get " + boss + " too 0% we win and we get loot.");
 console.log(leader + " initiates a Ready Check.");
 console.log("The Ready Cheack outputs " + readyCheck + ".");
 
-// If true
+// True Procedure
 if (readyCheck === true) {
     console.log("Ok we are ready and know the fight so let's get her down.");
     
- }else{
+ 
+//boolean expression
+
+var p = true, q = false, r = false;
+
+    console.log((p && !q) || (q && r));
+
     
-//False statment
-if(readyCheck === false){
-    console.log("OK. Whats the hold up " + coleader + "? Why did you hit " + readyCheck + "?")
-    }
-};
+ 
+ 
+ 
+ 
+ 
     
 //while loop 75%
 
@@ -62,37 +68,17 @@ while (  health > 50) {
     console.log("She is down to" + health + "% health. She is landing.");
 
     
-//For loop
-
-for (var health = 100; health > 0; health-=5) {
-    console.log("She is down to " + health + "% health.");
-    
-};
 
 
-// boolean expression
-
-var p = true, q = false, r = false;
-
-    console.log("the fight is " + ((p&&q) || r));
-
-    console.log( p &&(q || r));
-
-    console.log((p && !q) || (q && r));
-
-    console.log(!(p ||r));
 
 //array
 
 var members = ["Bluphonex", "Skybrook", "bratt"];
-
-   for (var i=0; i< members.length; i++){
-    
+    for (var i=0; i< members.length; i++){
     console.log(members[i] + " has died.");
-    };
+};
 
     console.log("Oh No! We have " + members.length + " raiders died.");
-
     console.log("How did you guys die?");
     
 var respons = [
@@ -101,8 +87,14 @@ var respons = [
     "got hit with tail swipe."
 ];
 for (var i=0, j=members.length; i <j; i++) {
-console.log(members[i] + " " + respons[i]);
+    console.log(members[i] + " " + respons[i]);
 
+};
+
+}else{
+    
+//False Procedure
+console.log("OK. Whats the hold up " + coleader + "? Why did you hit " + readyCheck + "?")
 };
 
 
