5eb9dd4f0ec731be61ee7324b72ab04c8f7c00c9
Michael Eaton II <Thrilshadow@Michaels-MacBook-Pro.local>
2013-01-14 15:53:17 -0500

    Added Array.

--- Eaton-Michael-project2.js
+++ Eaton-Michael-project2.js
@@ -12,7 +12,6 @@ var raidMembers = 8
 var coleader = "JohnyB"
 var boss = "Onxyia"
 var adds = "Whelplings"
-var deaths = 3
 var wipes = 0
 var readyCheck = true
 var health = 100
@@ -20,7 +19,7 @@ var requiredRaiders = 10
 
 //output
 
-console.log("After we got the required " + raidMembers + ", we are finally ready to go take on " + boss + ".")
+console.log("After we got the required " + raidMembers + ", We are finally ready to go take on " + boss + ".");
 console.log("Ok" + boss + " has three phases. Phase one will be a simple ground phase. Phase 2 is a sky phase.");
 console.log("In this phase range dps needs to be on " + boss + " and melee needs to be on " + adds + " control.");
 console.log("In phase 3 " + boss + " in back on the ground she will fear so everyone stay away from the " + adds + " caves."); 
@@ -35,8 +34,9 @@ if (readyCheck === true) {
  }else{
     
 //False statment
-if(readyCheck === false)
+if(readyCheck === false){
     console.log("OK. Whats the hold up " + coleader + "? Why did you hit " + readyCheck + "?")
+    }
 };
     
 //while loop 75%
@@ -70,7 +70,40 @@ for (var health = 100; health > 0; health-=5) {
 };
 
 
+// boolean expression
 
+var p = true, q = false, r = false;
+
+    console.log("the fight is " + ((p&&q) || r));
+
+    console.log( p &&(q || r));
+
+    console.log((p && !q) || (q && r));
+
+    console.log(!(p ||r));
+
+//array
+
+var members = ["Bluphonex", "Skybrook", "bratt"];
+
+   for (var i=0; i< members.length; i++){
+    
+    console.log(members[i] + " has died.");
+    };
+
+    console.log("Oh No! We have " + members.length + " raiders died.");
+
+    console.log("How did you guys die?");
+    
+var respons = [
+    "got feared into whelp cave.",
+    "got hit with fire breath.",
+    "got hit with tail swipe."
+];
+for (var i=0, j=members.length; i <j; i++) {
+console.log(members[i] + " " + respons[i]);
+
+};
 
 
 
